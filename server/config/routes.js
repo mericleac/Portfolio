b35565b8b9e9382ea281035a3a0484f620a32393
Mandy Mericle <33289865+mericleac@users.noreply.github.com>
2018-10-11 11:57:00 -0500

    Added catch-all route in routes.js

--- server/config/routes.js
+++ server/config/routes.js
@@ -4,4 +4,8 @@ module.exports = function (app) {
     app.post("/email", (req, res) => {
         emails.sendEmail(req, res);
     });
+
+    app.all("*", (req,res,next) => {
+        res.sendFile(path.resolve("./public/dist/public/index.html"))
+    });
 }
